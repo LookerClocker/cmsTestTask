88f855e7ad9e6133a5ab4d55cdeeb41e8b3b9bcc
okushnir <okushnir@grossum.com>
2017-11-09 12:03:24 +0200

    added onEnter

--- README.md
+++ README.md
@@ -10,5 +10,5 @@ Important:
 i couldn`t fetch data from local .json file using fetch method, so i decided do not waste time
 and have done it using "import".
 
-In code you will find some commented strings they are for those case when "fetch" will work.
+In code you will find "AppUsingFetch.js" file, which is for those case when "fetch" will work.
 It`s example of how i will handling data flow in case when i will use "fetch" method. 
--- src/AppUsingFetch.js
+++ src/AppUsingFetch.js
@@ -1,26 +1,54 @@
 import React, {Component} from 'react';
 import './App.css';
 import {Link} from 'react-router'
-import tabs from './tabs.json';
 
-class App extends Component {
+class AppUsingFetch extends Component {
     constructor(props) {
         super(props);
+
         this.state = {
-            tabs: [],
-            childrenWithProps: ''
+            childrenWithProps: '',
+            sortedTabs: []
         };
 
         this.sendData = this.sendData.bind(this);
     }
 
     componentDidMount(){
-        this.sendData(tabs[0])
+
+        fetch('./tabs', {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+            .then(response => {
+                if(response.status === 200) {
+                    return response.json();
+                }
+            })
+            .then(response => {
+                response.sort((prev, next) => {
+                    if (prev.order > next.order) {return 1;}
+                    if (prev.order < next.order) {return -1;}
+                });
+
+                this.setState({
+                    sortedTabs: response
+                });
+
+             this.sendData(response[0]);
+
+            })
+            .catch(error => {
+                throw (error)
+            });
+
     }
 
     /**
      * passing necessary props to specific child element
-     * **/
+     **/
     sendData(tab) {
         this.setState({
             childrenWithProps: React.Children.map(this.props.children,
@@ -29,6 +57,7 @@ class App extends Component {
                 })
             )
         });
+
     }
 
     render() {
@@ -42,12 +71,11 @@ class App extends Component {
                     <table>
                         <thead>
                         <tr>
-                            {tabs.sort((prev, next) => {
-                                if (prev.order > next.order) {return 1;}
-                                if (prev.order < next.order) {return -1;}
-                            }).map((tab) => <Link to={tab.id} onClick={() => this.sendData(tab)}>
-                                <td key={tab.id}>{tab.title}</td>
+                            {this.state.sortedTabs.map((tab) =>
+                            <Link to={tab.id} onClick={() => this.sendData(tab)}>
+                            <td key={tab.id}>{tab.title}</td>
                             </Link>)}
+
                         </tr>
                         </thead>
                         <tbody>
@@ -61,4 +89,4 @@ class App extends Component {
     }
 }
 
-export default App;
+export default AppUsingFetch;
